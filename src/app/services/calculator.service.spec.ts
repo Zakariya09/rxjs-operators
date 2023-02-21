3cb7958851d3a66afbfde0fe3f13881b63f3d18f
Zakariya Khan <zakariyakhan1@gmail.com>
2023-02-21 17:07:32 +0530

    Configuring TestBed For Service Testing

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -1,16 +1,29 @@
 import { CalculatorService } from "./calculator.service"
 import { LoggerService } from "./logger.service";
+import { TestBed } from '@angular/core/testing'
 
 describe("Calculator Service test suit", () => {
   let mockService: any;
   let calculator: CalculatorService;
 
+  let loggerService: jasmine.SpyObj<LoggerService>;
+
   beforeEach(() => {
     //to mock service i.e creating dummy instance of service
     mockService = jasmine.createSpyObj('LoggerService', ['log']);
-    calculator = new CalculatorService(mockService);
 
-  })
+    TestBed.configureTestingModule({
+      providers: [CalculatorService,
+        {
+          provide: LoggerService,
+          useValue: mockService
+        }]
+    });
+    calculator = TestBed.inject(CalculatorService);
+
+    //we can create instance of dependent service or child service
+    loggerService = TestBed.inject(LoggerService) as jasmine.SpyObj<LoggerService>;
+  });
 
   it("should add two number", () => {
 
@@ -23,13 +36,13 @@ describe("Calculator Service test suit", () => {
     let result = calculator.add(1, 2);
     expect(result).toBe(3)
     //alowing to visit the log function at once only
-    expect(mockService.log).toHaveBeenCalledTimes(1);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   })
 
   //using xit can disable test
   it("should substract two number", () => {
     let result = calculator.substract(2, 1);
     expect(result).toBe(1)
-    expect(mockService.log).toHaveBeenCalledTimes(1);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   })
 })
