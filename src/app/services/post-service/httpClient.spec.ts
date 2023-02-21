dc221e12191d9d50d69a3cd5581797e3d99f0797
Zakariya Khan <zakariyakhan1@gmail.com>
2023-02-21 22:47:55 +0530

    Testing Multiple API Calls using HttpTestingController

--- src/app/services/post-service/httpClient.spec.ts
+++ src/app/services/post-service/httpClient.spec.ts
@@ -9,7 +9,7 @@ describe("HttpClient Test Cases",
     () => {
         let httpClient: HttpClient;
         let httpTestingController: HttpTestingController;
-
+        let urlStr = '/data'
         beforeEach(() => {
             TestBed.configureTestingModule({
                 imports: [HttpClientTestingModule]
@@ -20,8 +20,8 @@ describe("HttpClient Test Cases",
         });
 
         it("should make api call with get request", () => {
-            const sampleData: Data= {name:"Zakariya Khan"};
-            httpClient.get<Data>('/data').subscribe((data)=>{
+            const sampleData: Data = { name: "Zakariya Khan" };
+            httpClient.get<Data>('/data').subscribe((data) => {
                 expect(data).toBe(sampleData);
             });
 
@@ -29,8 +29,34 @@ describe("HttpClient Test Cases",
             const request = httpTestingController.expectOne('/data');
             //returning sample data as response to http GET request
             request.flush(sampleData);
-            
+
             //Verifying method which method is call for the given URL
             expect(request.request.method).toBe('GET');
         });
+
+
+        //multiple API call testing
+        it("should make multiple api calls", () => {
+            const sampleData: Data[] = [{ name: 'request 1' }, { name: 'request 2' }];
+
+            httpClient.get<Data[]>(urlStr).subscribe((data) => {
+                expect(data?.length).toBe(0);
+            });
+            httpClient.get<Data[]>(urlStr).subscribe((data) => {
+                expect(data).toEqual([sampleData[0]])
+            });
+            httpClient.get<Data[]>(urlStr).subscribe((data) => {
+                expect(data).toEqual(sampleData);
+            });
+
+            //match use to detect multiple api calls on same url request
+            const requests = httpTestingController.match(urlStr);
+
+            expect(requests.length).toBe(3);
+
+            //sending dummy reponse to the given URL
+            requests[0].flush([]);
+            requests[1].flush([sampleData[0]]);
+            requests[2].flush(sampleData);
+        })
     });
